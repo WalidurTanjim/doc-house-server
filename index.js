d799c076b6aa900a62dc14d51a48ec1b0480c963
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-11-27 22:30:24 +0600

    Cloudinary added

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const cloudinary = require('./cloudinary/cloudinary');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cookieParser());
 
 
 
@@ -20,49 +23,62 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-        // collections
-        const doctorsCollection = client.db('DocHouse').collection('doctors');
+    // collections
+    const doctorsCollection = client.db('DocHouse').collection('doctors');
 
-        // doctors related api
-        app.get('/doctors/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await doctorsCollection.findOne(query);
-            res.send(result);
-        });
+    // doctors related api
+    app.get('/doctors/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await doctorsCollection.findOne(query);
+        res.send(result);
+    });
 
-        app.get('/doctors', async(req, res) => {
-            const result = await doctorsCollection.find().toArray();
-            res.send(result);
-        });
+    app.get('/doctors', async(req, res) => {
+        const result = await doctorsCollection.find().toArray();
+        res.send(result);
+    });
 
-        app.delete('/doctors/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await doctorsCollection.deleteOne(query);
-            res.send(result);
-        });
-
-
-
+    app.delete('/doctors/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await doctorsCollection.deleteOne(query);
+        res.send(result);
+    });
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
-};
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
 run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Doc House server is running...');
 })
+
+app.post('/', async(req, res) => {
+  const { image } = req.body;
+  cloudinary.uploader
+      .upload(image, {
+          upload_preset: "images_preset",
+          allowed_formats: ['png', 'jpg', 'jpeg', 'svg', 'ico', 'jfif', 'webp']
+      })
+      .then(result => {
+          console.log(result);
+          res.status(200).send(result);
+      })
+});
 
 
 app.listen(port, () => {
